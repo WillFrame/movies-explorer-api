4bf64dae673fa5333b9bafdbf590988037f5dd78
WillFrame <32850788+WillFrame@users.noreply.github.com>
2022-07-13 11:43:19 +0300

    Delete middlewares directory

--- middlewares/error.js
+++
@@ -1,7 +0,0 @@
-const error = (err, req, res, next) => {
-  const { statusCode = 500, message } = err;
-  res.status(statusCode).send({ message: statusCode === 500 ? 'Произошла ошибка' : message });
-  next();
-};
-
-module.exports = error;
