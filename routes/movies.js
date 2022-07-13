a09ec8f8f6fdcc109a2b2235caed990057f0c2ba
WillFrame <32850788+WillFrame@users.noreply.github.com>
2022-07-13 11:43:37 +0300

    Delete routes directory

--- routes/movies.js
+++
@@ -1,9 +0,0 @@
-const movieRouter = require('express').Router();
-
-const { getMovies, createMovie, deleteMovie } = require('../controllers/movies');
-
-movieRouter.get('/', getMovies);
-movieRouter.post('/', createMovie);
-movieRouter.delete('/:movieId', deleteMovie);
-
-module.exports = movieRouter;
--- routes/users.js
+++
@@ -1,8 +0,0 @@
-const userRouter = require('express').Router();
-
-const { getUser, updateUser } = require('../controllers/users');
-
-userRouter.get('/me', getUser);
-userRouter.patch('/me', updateUser);
-
-module.exports = userRouter;
