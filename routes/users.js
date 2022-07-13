59bd4f409ddeaa637deb8a1f641f89f343f65d5b
WillFrame <32850788+WillFrame@users.noreply.github.com>
2022-07-13 15:47:25 +0700

    Add files via upload

---
+++ routes/users.js
@@ -0,0 +1,8 @@
+const userRouter = require('express').Router();
+
+const { getUser, updateUser } = require('../controllers/users');
+
+userRouter.get('/me', getUser);
+userRouter.patch('/me', updateUser);
+
+module.exports = userRouter;
